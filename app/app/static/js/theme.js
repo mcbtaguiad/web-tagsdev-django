6d663f3d7acf37ad033f6930815e516f7aba6667
mcbtaguiad <marktaguiad@gmail.com>
2022-11-28 04:12:05 -0500

    added spotify link, remove non relevant social

--- app/app/static/js/theme.js
+++ app/app/static/js/theme.js
@@ -7,27 +7,54 @@ const themes = {
   DARK: "dark",
 };
 
-initTheme();
+//initTheme();
+//
+
+initThemeLight();
+
+function initThemeLight() {
+  setTheme(themes.LIGHT);
+  document.getElementById("sunmoon").href = "https://www.imcollectingmoonlight.com/sunlightgallery/";
+
+
+}
+
 
 function initTheme() {
   const savedTheme = localStorage.getItem(STORAGE_KEY);
 
   if (savedTheme) {
     // Storage theme
-    setTheme(savedTheme);
-  } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
+    //setTheme(savedTheme);
+    if (savedTheme == themes.DARK) {
+      setTheme(themes.DARK);
+      document.getElementById("sunmoon").href = "https://www.imcollectingmoonlight.com/moonlightgallery/";
+
+    } else {
+      setTheme(themes.LIGHT);
+      document.getElementById("sunmoon").href = "https://www.imcollectingmoonlight.com/sunlightgallery/";
+
+    }
+
+  //} else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
     // system theme
-    setTheme(themes.DARK);
-  } else {
+  //  setTheme(themes.DARK);
+  //  document.getElementById("sunmoon").href = "https://www.imcollectingmoonlight.com/moonlightgallery/";
+    //document.getElementById("moonsun").className = "fas fa-moon fa-1x";
+
+
+  //} else {
     // Default theme
-    setTheme(themes.LIGHT);
+    //setTheme(themes.LIGHT);
+    //document.getElementById("sunmoon").href = "https://www.imcollectingmoonlight.com/sunlightgallery/";
+    //document.getElementById("moonsun").className = "fas fa-sun fa-1x";
   }
 
   // Watch for system theme changes
-  window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
-    const newTheme = e.matches ? themes.DARK : themes.LIGHT;
-    setTheme(newTheme);
-  });
+  //window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
+  //  const newTheme = e.matches ? themes.DARK : themes.LIGHT;
+  //  setTheme(newTheme);
+  //});
 }
 
 function toggleTheme() {
@@ -35,6 +62,21 @@ function toggleTheme() {
   const newTheme = theme === themes.DARK ? themes.LIGHT : themes.DARK;
   setTheme(newTheme);
   localStorage.setItem(STORAGE_KEY, newTheme);
+  //document.getElementById("moonsun").href="https://www.imcollectingmoonlight.com/moonlightgallery/";
+  if(document.getElementById("sunmoon").href == "https://www.imcollectingmoonlight.com/sunlightgallery/"){
+    document.getElementById("sunmoon").href = "https://www.imcollectingmoonlight.com/moonlightgallery/";
+    setTheme(themes.DARK)
+  }else{
+    document.getElementById("sunmoon").href = "https://www.imcollectingmoonlight.com/sunlightgallery/";
+    setTheme(themes.LIGHT)
+  }
+  //if(document.getElementById("moonsun").className == "fas fa-sun fa-1x"){
+  //  document.getElementById("moonsun").className = "fas fa-moon fa-1x";
+  //  setTheme(themes.DARK)
+  //}else{
+  //  document.getElementById("moonsun").className = "fas fa-sun fa-1x";
+  //  setTheme(themes.LIGHT)
+  //}
 }
 
 function getTheme() {
